ea14d84b80960eb016805f009d8415e1a4ad30c1
Dheeraj26-02 <dheeraj8483@gmail.com>
2025-08-27 15:02:18 +0530

    added in server file

--- src/main/java/com/example/em_project/EmProjectApplication.java
+++ src/main/java/com/example/em_project/EmProjectApplication.java
@@ -9,6 +9,14 @@ public class EmProjectApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(EmProjectApplication.class, args);
+		System.out.println(" ____   _                                        \n" +
+				"|  _ \\ | |__    ___   ___ _ __ __ _  __       \n" +
+				"| | | || '_ \\  / _ \\ / _ \\ '__/ _` ||  |     \n" +
+				"| |_| || | | ||  __/|  __/ | | (_| ||  |   \n" +
+				"|____/ |_| |_| \\___| \\___|_|  \\__,_||  |\n" +
+				"                                    |  |\n" +
+				"                                 __ /  /\n" +
+				"                                |____ / ");
 		System.out.println("Server running at "+"http://localhost:9000/");
 	}
 
